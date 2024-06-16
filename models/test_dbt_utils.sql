0bd7182466b7a2ffdd25c4008655ff204acec9f0
anil kumar <challaanil16@gmail.com>
2024-06-16 02:16:56 +0000

    files added

--- models/test_dbt_utils.sql
+++ models/test_dbt_utils.sql
@@ -1,2 +1,5 @@
 {% set DEPARTMENT_LIST = dbt_utils.get_column_values(source("test_dbt", "EMPLOYEES"), column='DEPARTMENT_ID') %}
 {{DEPARTMENT_LIST}}
+{% for DEPARTMENT in DEPARTMENT_LIST %}
+{{DEPARTMENT}}
+{% endfor %}
